ba293e3a813906157bfd779b993d4066370f6ce1
Javier Barrera <js.barrerat1@uniandes.edu.co>
2024-03-25 11:28:52 -0500

    Add @Table notation (is necesary because the connection generates new tables, e.g. book_entity for BookEntity class)

--- src/main/java/com/library/entities/AuthorEntity.java
+++ src/main/java/com/library/entities/AuthorEntity.java
@@ -2,9 +2,11 @@ package com.library.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+@Table(name = "author")
 @Data
 public class AuthorEntity {
   @Id
--- src/main/java/com/library/entities/CommentEntity.java
+++ src/main/java/com/library/entities/CommentEntity.java
@@ -2,9 +2,11 @@ package com.library.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+@Table(name = "comment")
 @Data
 public class CommentEntity {
   @Id
